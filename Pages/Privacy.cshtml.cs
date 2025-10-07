409098c0683f26d39f853a64689ecbd0dde41562
kirah-cox <kirahanncox@gmail.com>
2025-10-07 12:21:40 -0600

    finished assignment

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ajax_API_Dad_Jokes.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
---
+++ wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿window.onload = function(){
+    getDadJoke();
+}
+
+function getDadJoke(){
+    let xhr = new XMLHttpRequest();
+    let url = 'https://icanhazdadjoke.com';
+
+    xhr.open('GET', url, true);
+    
+    xhr.setRequestHeader('Accept', 'application/json');
+
+    xhr.onload = function () {
+        $('#myButton').click(function() {
+            if (xhr.status == 200){
+    
+                let data = JSON.parse(xhr.responseText);
+    
+                document.getElementById('result').innerHTML = `
+                    <p>${data.joke}</p>
+                `;
+            }
+            else {
+                document.getElementById('result').innerHTML = 'Error: ' + xhr.status;
+            }
+        });
+    }
+    xhr.send();
+}
